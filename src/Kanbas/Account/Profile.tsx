fa59be87ff66f35695557e163896b6b5a6aa7ccc
Tianshu Chu <scarlett@Tianshus-MacBook-Pro.local>
2024-06-19 18:20:58 -0400

    a6

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -33,39 +33,46 @@ export default function Profile() {
       <h1>Profile</h1>
       {profile && (
         <div>
+          <h5 className="mt-3">username</h5>
           <input
             value={profile.username}
             onChange={(e) =>
               setProfile({ ...profile, username: e.target.value })
             }
           />
+          <h5 className="mt-3">password</h5>
           <input
             value={profile.password}
             onChange={(e) =>
               setProfile({ ...profile, password: e.target.value })
             }
           />
+          <h5 className="mt-3">first name</h5>
           <input
             value={profile.firstName}
             onChange={(e) =>
               setProfile({ ...profile, firstName: e.target.value })
             }
           />
+          <h5 className="mt-3">last name</h5>
           <input
             value={profile.lastName}
             onChange={(e) =>
               setProfile({ ...profile, lastName: e.target.value })
             }
           />
+          <h5 className="mt-3">date of birth</h5>
           <input
             value={profile.dob}
             onChange={(e) => setProfile({ ...profile, dob: e.target.value })}
             type="date"
           />
+          <h5 className="mt-3">email</h5>
           <input
             value={profile.email}
             onChange={(e) => setProfile({ ...profile, email: e.target.value })}
           />
+          <h5 className="mt-3">role</h5>
           <select
             onChange={(e) => setProfile({ ...profile, role: e.target.value })}
           >
@@ -74,7 +81,7 @@ export default function Profile() {
             <option value="FACULTY">Faculty</option>{" "}
             <option value="STUDENT">Student</option>
           </select>
-          <button onClick={signout} className="btn btn-danger w-100">
+          <button onClick={signout} className="btn btn-danger w-100 mt-3">
             Sign out
           </button>
         </div>
