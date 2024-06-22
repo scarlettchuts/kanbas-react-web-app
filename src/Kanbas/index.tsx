493489f6f6a3aaf125bbfc86960e9172e810ff41
Tianshu Chu <scarlett@Tianshus-MacBook-Pro.local>
2024-06-21 20:37:38 -0400

    first commit

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -10,6 +10,7 @@ import store from "./store";
 import { Provider } from "react-redux";
 import Account from "./Account";
 import ProtectedRoute from "./ProtectedRoute";
+import Session from "./Account/Session";
 
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
@@ -56,44 +57,46 @@ export default function Kanbas() {
 
   return (
     <Provider store={store}>
-      <div id="wd-kanbas" className="">
-        <div className="">
-          <div className="d-none d-md-block bg-black sticky-side-navigation">
-            <KanbasNavigation />
-          </div>
-          <div className="custom-margins">
-            <Routes>
-              <Route path="/" element={<Navigate to="Dashboard" />} />
-              <Route path="/Account/*" element={<Account />} />
-              <Route
-                path="Dashboard"
-                element={
-                  <ProtectedRoute>
-                    <Dashboard
-                      courses={courses}
-                      course={course}
-                      setCourse={setCourse}
-                      addNewCourse={addNewCourse}
-                      deleteCourse={deleteCourse}
-                      updateCourse={updateCourse}
-                    />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="Courses/:cid/*"
-                element={
-                  <ProtectedRoute>
-                    <Courses courses={courses} />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="Calendar" element={<h1>Calendar</h1>} />
-              <Route path="Inbox" element={<h1>Inbox</h1>} />
-            </Routes>
+      <Session>
+        <div id="wd-kanbas" className="">
+          <div className="">
+            <div className="d-none d-md-block bg-black sticky-side-navigation">
+              <KanbasNavigation />
+            </div>
+            <div className="custom-margins">
+              <Routes>
+                <Route path="/" element={<Navigate to="Dashboard" />} />
+                <Route path="/Account/*" element={<Account />} />
+                <Route
+                  path="Dashboard"
+                  element={
+                    <ProtectedRoute>
+                      <Dashboard
+                        courses={courses}
+                        course={course}
+                        setCourse={setCourse}
+                        addNewCourse={addNewCourse}
+                        deleteCourse={deleteCourse}
+                        updateCourse={updateCourse}
+                      />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="Courses/:cid/*"
+                  element={
+                    <ProtectedRoute>
+                      <Courses courses={courses} />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="Calendar" element={<h1>Calendar</h1>} />
+                <Route path="Inbox" element={<h1>Inbox</h1>} />
+              </Routes>
+            </div>
           </div>
         </div>
-      </div>
+      </Session>
     </Provider>
   );
 }
